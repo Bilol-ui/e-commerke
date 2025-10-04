9fe8a4b07d498b00657fa6020e861b9d64ec8f87
bilol <yolchixojayevbilol6@gmail.com>
2025-10-04 09:26:21 +0500

    Yangi o‘zgartirishlar qo‘shildi

--- apps/models.py
+++ apps/models.py
@@ -1,3 +1,29 @@
-from django.db import models
+from datetime import timedelta
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db.models import EmailField, CharField, BooleanField, Model, OneToOneField, CASCADE, DateTimeField, \
+    IntegerField
+from django.utils.timezone import now
+
+
+class User(AbstractUser):
+    username = None
+    email = EmailField(unique=True, null=True, blank=True)
+    phone = CharField(max_length=20, unique=True, null=True, blank=True)
+    is_verified = BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email or self.phone
+
+
+class PhoneVerification(Model):
+    user = OneToOneField(User, CASCADE)
+    code = CharField(max_length=6)
+    created_at = DateTimeField(auto_now_add=True)
+    attempts = IntegerField(default=0)
+
+    def is_expired(self):
+        return now() > self.created_at + timedelta(minutes=1)
